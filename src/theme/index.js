d6db6467ce04f883dc4195f1684b656ff465af73
Yago Ribeiro <yagoribeiro21@gmail.com>
2021-05-01 13:32:47 -0300

    Thema

---
+++ src/theme/index.js
@@ -0,0 +1,70 @@
+export const defaultTheme = {
+  name: 'agroceo',
+  roundness: 8,
+  colors: {
+    primary: '#02733E',
+    primaryVariant: '#F39200',
+    secondary: '#EA690A',
+    secondaryAccent: '#F39200',
+    background: '#FFFFFF',
+    surface: '#F5F5F7',
+    success: '#018849',
+    error: '#ED1C24',
+    alert: '#FFD400',
+    neutral0: '#FFFFFF',
+    neutral1: '#F5F5F7',
+    neutral2: '#E5E5E5',
+    neutral3: '#C0C0C0',
+    neutral4: '#7C7B7B',
+    neutral5: '#212121',
+    neutral6: '#A3A3A3',
+    events: {
+      onPrimary: '#FFFFFF',
+      onPrimaryVariant: '#FFFFFF',
+      onSecondary: '#FFFFFF',
+      onSecondaryAccent: '#FFFFFF',
+      onBackground: '#212121',
+      onSurface: '#212121',
+      onSuccess: '#FFFFFF',
+      onError: '#FFFFFF',
+      onAlert: '#212121',
+      onNeutral0: '#212121',
+      onNeutral1: '#212121',
+      onNeutral2: '#212121',
+      onNeutral3: '#212121',
+      onNeutral4: '#FFFFFF',
+      onNeutral5: '#FFFFFF',
+    },
+  },
+  text: {
+    size: {
+      h1: '96px',
+      h2: '64px',
+      h3: '48px',
+      h4: '32px',
+      h5: '24px',
+      large: '18px',
+      medium: '16px',
+      normal: '14px',
+      small: '13px',
+      caption: '12px',
+      extraSmall: '11px',
+      mini: '10px',
+      label: '7px',
+    },
+    type: {
+      lineThrough: 'line-through',
+      underline: 'underline',
+    },
+    fontFamily: {
+      bold: 'Inter-Bold',
+      regular: 'Inter-Regular',
+    },
+  },
+  fonts: {
+    regular: 'Inter-Regular'
+  },
+  animation: {
+    scale: 1.0
+  }
+};
